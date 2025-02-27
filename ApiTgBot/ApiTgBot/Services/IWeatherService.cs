bbe492e24aa057e2b25b2a2d8857cbabd5687861
thatshowmafiaworks <shinehd56@gmail.com>
2025-02-27 18:46:07 +0200

    added WeatherService

---
+++ ApiTgBot/ApiTgBot/Services/IWeatherService.cs
@@ -0,0 +1,9 @@
+﻿using ApiTgBot.Models.DTOs;
+
+namespace ApiTgBot.Services
+{
+    public interface IWeatherService
+    {
+        Task<string> GetForecast(CoordinatesDto coordinates);
+    }
+}
---
+++ ApiTgBot/ApiTgBot/Services/WeatherService.cs
@@ -0,0 +1,22 @@
+﻿using ApiTgBot.Data;
+using ApiTgBot.Models.DTOs;
+using static System.Net.WebRequestMethods;
+
+namespace ApiTgBot.Services
+{
+    public class WeatherService(IConfiguration config, IDbContext context): IWeatherService
+    {
+        public async Task<string> GetForecast(CoordinatesDto coordinates)
+        {
+            var apiKey = config["OpenWeatherToken"];
+            var url = $"https://api.openweathermap.org/data/3.0/onecall/overview?lat={coordinates.Lat}&lon={coordinates.Lng}&appid={apiKey}";
+            using var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody);
+            return "Aboba";
+        }
+    }
+}
